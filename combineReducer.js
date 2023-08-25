f98fb3378ad74de42f9ca074090e2440989dc892
sumit <mazumdarsumit3@gmail.com>
2023-08-25 14:45:23 +0530

    redux -> bindActionCreators

--- combineReducer.js
+++ combineReducer.js
@@ -1,10 +1,11 @@
-import { createStore, bindActionCreators } from 'redux';
+import { createStore, bindActionCreators, combineReducers } from 'redux';
 
 const ADD_TODO = 'add-todo';
 const DEL_TODO = 'delete-todo';
 const UPD_TODO = 'edit_todo';
+const ADD_USER = 'add_user';
 
-function todoReducer(state, action) {
+function todoReducer(state = [], action) {
   if (action.type == ADD_TODO) {
     const todoText = action.payload.todoText;
     return [
@@ -30,32 +31,35 @@ function todoReducer(state, action) {
   return state;
 }
 
-// action objects -> action methods(action creator)
-const addTodo = (todoText) => ({ type: ADD_TODO, payload: { todoText } });
-const deleteTodo = (id) => ({ type: DEL_TODO, payload: { todoId: id } });
+function useReducer(state = [], action) {
+  if (action.type == ADD_USER) {
+    const todoText = action.payload.userName;
+    return [
+      ...state,
+      {
+        name: userName,
+        isFinished: false,
+        id: state.length == 0 ? 1 : state[state.length - 1].id + 1,
+      },
+    ];
+  }
+  return state;
+}
 
+// action objects -> action methods(action creator)
+const addTodo = () => ({ type: ADD_TODO, payload: { todoText: 'todo 1' } });
 const { dispatch, subscribe, getState, replaceReducer } = createStore(
   todoReducer,
   []
 );
-subscribe(() => console.log(getState())); // executes this callback
-
-const actions = bindActionCreators({ addTodo, deleteTodo }, dispatch);
+subscribe(() => console.log(getState()));
 
 // dispatch({ type: ADD_TODO, payload: { todoText: 'todo 1' } });
-// console.log(getState()); subscribe is doing this work
+// // console.log(getState()); subscribe is doing this work
 
 // dispatch({ type: ADD_TODO, payload: { todoText: 'todo 2' } });
 //console.log(getState());
 
-//dispatch({ type: DEL_TODO, payload: { todoId: 1 } });
+dispatch(add);
+dispatch({ type: DEL_TODO, payload: { todoId: 1 } });
 //console.log(getState());
-
-// dispatch(addTodo('todo 1')); after bindActionCreators
-// dispatch(addTodo('todo 2'));
-// dispatch(deleteTodo(1));
-
-actions.addTodo('todo 1');
-actions.addTodo('todo 2');
-
-actions.deleteTodo(1);
